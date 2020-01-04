5be1a0f48606d912c25435f94646dd7b1a77a3df
Thi Nguyen <thiman@gmail.com>
2020-01-03 23:45:10 -0800

    added sass. Implemented edit and delete in jquery

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 const students = [
   {
-    id: 'aa',
+    id: 1,
     grade: 68,
     name: 'Joseph'
   },
   {
-    id: 'ab',
+    id: 2,
     grade: 100,
     name: 'Howard'
   },
   {
-    id: 'ac',
+    id: 3,
     grade: 79,
     name: 'Tan'
   },
   {
-    id: 'ad',
+    id: 4,
     grade: 88,
     name: 'Matt'
   },
   {
-    id: 'ae',
+    id: 5,
     grade: 90,
     name: 'Jessie'
   },
   {
-    id: 'af',
+    id: 6,
     grade: 95,
     name: 'Han'
   },
@@ -33,21 +33,43 @@ const students = [
 
 const toggleBtn = document.querySelector('#mode-toggle-btn');
 const bodyEle = document.querySelector('body');
-const studentTable = document.querySelector('#student-grade-table');
-const studentTableBody = studentTable.querySelector('tbody');
+const studentTable = $('#student-grade-table');
+const studentTableBody = studentTable.find('tbody');
 
-const studentTableFooter = studentTable.querySelector('tfoot');
-const newStudentNameInput = studentTableFooter.querySelector('#new-student-name');
-const newStudentGradeInput = studentTableFooter.querySelector('#new-student-grade');
-const newStudentAddBtn = studentTableFooter.querySelector('#add-btn');
+const studentTableFooter = studentTable.find('tfoot');
+const newStudentNameInput = studentTableFooter.find('#new-student-name');
+const newStudentGradeInput = studentTableFooter.find('#new-student-grade');
+const newStudentAddBtn = studentTableFooter.find('#add-btn');
+
+const editForm = $('#edit-form');
 
 let isDarkMode = false;
+let currentEditedRow = null;
+let currentEditedStudentId = null;
 
 /**
  * ===================================================================
  * EVENT LISTENERS
  * ===================================================================
  */
+editForm.on('submit', function(event) {
+  // Stop the usual submission process
+  event.preventDefault();
+
+  const formData = new FormData(editForm[0]);
+  const edited = {
+    id: Number(formData.get('id')),
+    name: formData.get('name'),
+    grade: Number(formData.get('grade')),
+  }
+  
+  const index = students.findIndex(student => student.id === edited.id);
+  students[index] = edited;
+  
+  currentEditedRow.html(generateRow(edited).html());
+  currentEditedRow = null;
+  currentEditedStudentId = null;
+});
 
 toggleBtn.addEventListener('click', function(event) {
   bodyEle.classList.toggle('dark');
@@ -55,7 +77,7 @@ toggleBtn.addEventListener('click', function(event) {
   this.textContent = isDarkMode ? 'Light Mode' : 'Dark Mode';
 });
 
-newStudentAddBtn.addEventListener('click', function(event) {
+newStudentAddBtn.on('click', function(event) {
   const newName = newStudentNameInput.value;
   const newGrade = newStudentGradeInput.value;
   const newId = Date.now();
@@ -90,14 +112,35 @@ newStudentAddBtn.addEventListener('click', function(event) {
 });
 
 // Event delegation on the tbody element. The block below looks for elements with the specified class to act on
-studentTableBody.addEventListener('click', function(event) {
-  if (event.srcElement.classList.contains('delete-btn')) {
-    // delete
-    studentTableBody.removeChild(event.srcElement.parentNode.parentNode.parentNode.parentNode);
-  } else if (event.srcElement.classList.contains('edit-btn')) {
-    // edit
-  }
-});
+studentTableBody
+  .on('click', '.delete-btn', function(event) {
+    const row = $(event.target).parents('tr');
+    const studentId = Number(row.attr('data-id'));
+    row.animate({
+      opacity: 0
+    }, {
+      duration: 500,
+      complete: function() {
+        row.remove();
+        const index = students.findIndex(student => student.id === studentId);
+        students.splice(index, 1);
+      }
+    });
+  })
+  .on('click', '.edit-btn', function(event) {
+    const row = $(event.target).parents('tr');
+    currentEditedRow = row;
+    const student = students.find(student => student.id === Number(row.attr('data-id')));
+    currentEditedStudentId = student.id;
+    
+    row.html(getEditTemplate(student));
+  })
+  .on('click', '#edit-cancel-btn', function(event) {
+    const student = students.find(student => student.id === currentEditedStudentId);
+    currentEditedRow.html(generateRow(student).html());
+    currentEditedStudentId = null;
+    currentEditedRow = null;
+  })
 
 /**
  * ===================================================================
@@ -112,30 +155,41 @@ const optionsTemplate = `<div class="options-container">
 <li class="delete-btn">Delete</li>
 </ul></div>`;
 
+const getEditTemplate = function(student) {
+  return `
+    <td><input type="text" name="name" autocomplete="false" placeholder="Student Name" value=${student.name} form="edit-form" /></td>
+    <td><input type="number" name="grade" autocomplete="false" placeholder="Student Name" value=${student.grade} form="edit-form" /></td>
+    <td>
+      <button type="submit" form="edit-form">Save</button>
+      <button type="reset" form="edit-form">Reset</button>
+      <button type="button" form="edit-form" id="edit-cancel-btn">Cancel</button>
+      <input type="hidden" name="id" value="${student.id}" form="edit-form" />
+    </td>
+  `;
+}
+
+function generateRow(student) {
+  const row = $('<tr/>').attr('data-id', student.id);
+    const nameCol = $('<td/>').addClass('name-col').text(student.name);
+    const gradeCol = $('<td/>').addClass('grade-col').text(student.grade);
+    const optionsCol = $('<td/>').addClass('options-col').html(optionsTemplate);
+
+    row
+      .append(nameCol)
+      .append(gradeCol)
+      .append(optionsCol);
+
+    return row;
+}
+
 function renderStudentTable() {
-  studentTableBody.innerHTML = '';
+  studentTableBody[0].innerHTML = '';
 
   for(let i = 0; i < students.length; i++) {
     const student = students[i];
-    const row = document.createElement('tr');
-    const nameCol = document.createElement('td');
-    const gradeCol = document.createElement('td');
-    const optionsCol = document.createElement('td');
-
-    nameCol.textContent = student.name;
-    nameCol.className = 'name-col';
-    gradeCol.textContent = student.grade;
-    gradeCol.className = 'grade-col';
-    optionsCol.className = 'options-col';
-    optionsCol.innerHTML = optionsTemplate;
-
-    row.appendChild(nameCol);
-    row.appendChild(gradeCol);
-    row.appendChild(optionsCol);
-    studentTableBody.appendChild(row);
+    
+    generateRow(student).appendTo(studentTableBody);
   }
 }
-
-
 
 renderStudentTable();
