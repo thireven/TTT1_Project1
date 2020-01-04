b013486719d51d004780e5b9d4b7b3a76ef406fb
Thi Nguyen <thiman@gmail.com>
2020-01-04 09:44:27 -0800

    sort implemented

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const students = [
   {
     id: 4,
     grade: 88,
-    name: 'Matt'
+    name: 'How'
   },
   {
     id: 5,
@@ -35,6 +35,7 @@ const toggleBtn = document.querySelector('#mode-toggle-btn');
 const bodyEle = document.querySelector('body');
 const studentTable = $('#student-grade-table');
 const studentTableBody = studentTable.find('tbody');
+const studentTableHeader = studentTable.find('thead');
 
 const studentTableFooter = studentTable.find('tfoot');
 const newStudentNameInput = studentTableFooter.find('#new-student-name');
@@ -46,6 +47,38 @@ const editForm = $('#edit-form');
 let isDarkMode = false;
 let currentEditedRow = null;
 let currentEditedStudentId = null;
+let currentSortColumn = null;
+
+function sortByName(dir = 1) {
+  return function(a, b) {
+    const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1 * dir;
+    }
+    if (nameA > nameB) {
+      return 1 * dir;
+    }
+
+    // names must be equal
+    return 0;
+  }
+}
+
+function sortByGrade(dir) {
+  return function(a, b) {
+    const gradeA = Number(a.grade);
+    const gradeB = Number(b.grade);
+    if (gradeA < gradeB) {
+      return -1 * dir;
+    }
+    if (gradeA > gradeB) {
+      return 1 * dir;
+    }
+
+    return 0;
+  }
+}
 
 /**
  * ===================================================================
@@ -62,13 +95,56 @@ editForm.on('submit', function(event) {
     name: formData.get('name'),
     grade: Number(formData.get('grade')),
   }
-  
+
   const index = students.findIndex(student => student.id === edited.id);
   students[index] = edited;
-  
+
   currentEditedRow.html(generateRow(edited).html());
   currentEditedRow = null;
   currentEditedStudentId = null;
+});
+
+studentTableHeader.find('.sortable').on('click', function(event) {
+  const sortBy = $(this).attr('data-sort');
+  const sortDirStr = $(this).attr('data-sort-dir');
+
+  let sortDir = 1;
+  let sortIcon = '';
+
+  if (currentSortColumn !== null) {
+    currentSortColumn.find('.dir-icon').text('');
+  }
+
+  currentSortColumn = $(this);
+
+  switch(sortDirStr) {
+    case '':
+    case '-1':
+      sortDir = 1;
+      sortIcon = '&and;';
+      break;
+    case '1':
+    default:
+      sortDir = -1;
+      sortIcon = '&or;';
+      break;
+  }
+
+  $(this).attr('data-sort-dir', sortDir);
+
+  let sortFn = null;
+  switch(sortBy) {
+    case 'name':
+      sortFn = sortByName(sortDir);
+      break;
+    case 'grade':
+      sortFn = sortByGrade(sortDir);
+      break;
+  }
+
+  $(this).find('.dir-icon').html(sortIcon);
+  students.sort(sortFn);
+  renderStudentTable();
 });
 
 toggleBtn.addEventListener('click', function(event) {
@@ -111,7 +187,6 @@ newStudentAddBtn.on('click', function(event) {
   renderStudentTable();
 });
 
-// Event delegation on the tbody element. The block below looks for elements with the specified class to act on
 studentTableBody
   .on('click', '.delete-btn', function(event) {
     const row = $(event.target).parents('tr');
@@ -132,7 +207,7 @@ studentTableBody
     currentEditedRow = row;
     const student = students.find(student => student.id === Number(row.attr('data-id')));
     currentEditedStudentId = student.id;
-    
+
     row.html(getEditTemplate(student));
   })
   .on('click', '#edit-cancel-btn', function(event) {
@@ -151,8 +226,8 @@ studentTableBody
 const optionsTemplate = `<div class="options-container">
 <strong>MENU</strong>
 <ul class="options-list">
-<li class="edit-btn">Edit</li>
-<li class="delete-btn">Delete</li>
+  <li class="edit-btn">Edit</li>
+  <li class="delete-btn">Delete</li>
 </ul></div>`;
 
 const getEditTemplate = function(student) {
@@ -170,16 +245,16 @@ const getEditTemplate = function(student) {
 
 function generateRow(student) {
   const row = $('<tr/>').attr('data-id', student.id);
-    const nameCol = $('<td/>').addClass('name-col').text(student.name);
-    const gradeCol = $('<td/>').addClass('grade-col').text(student.grade);
-    const optionsCol = $('<td/>').addClass('options-col').html(optionsTemplate);
+  const nameCol = $('<td/>').addClass('name-col').text(student.name);
+  const gradeCol = $('<td/>').addClass('grade-col').text(student.grade);
+  const optionsCol = $('<td/>').addClass('options-col').html(optionsTemplate);
 
-    row
-      .append(nameCol)
-      .append(gradeCol)
-      .append(optionsCol);
+  row
+    .append(nameCol)
+    .append(gradeCol)
+    .append(optionsCol);
 
-    return row;
+  return row;
 }
 
 function renderStudentTable() {
@@ -187,7 +262,7 @@ function renderStudentTable() {
 
   for(let i = 0; i < students.length; i++) {
     const student = students[i];
-    
+
     generateRow(student).appendTo(studentTableBody);
   }
 }
